015724f5311dd689b2eff3e9253385ed50cdaba0
Lars Rustand <rustand.lars@gmail.com>
2023-12-01 21:07:04 +0100

    Add home-channels-service

--- home.scm
+++ home.scm
@@ -3,6 +3,7 @@
              (gnu services)
              (gnu packages web-browsers)
              (guix gexp)
+             (guix channels)
              (lrustand packages package-groups)
              (lrustand home services)
              (lrustand mail offlineimap)
@@ -14,7 +15,8 @@
  mail
  syncthing
  desktop
- shepherd)
+ shepherd
+ guix)
 
 
 (define email-accounts
@@ -64,6 +66,29 @@
  
  (services
   (list
+   (simple-service 'nonguix-channel-service
+                   home-channels-service-type
+                   (list
+                    (channel
+                     (name 'nonguix)
+                     (url "https://gitlab.com/nonguix/nonguix")
+                     ;; Enable signature verification:
+                     (introduction
+                      (make-channel-introduction
+                       "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
+                       (openpgp-fingerprint
+                        "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))))
+   (simple-service 'rde-channel-service
+                   home-channels-service-type
+                   (list
+                    (channel
+                     (name 'rde)
+                     (url "https://git.sr.ht/~abcdw/rde")
+                     (introduction
+                      (make-channel-introduction
+                       "257cebd587b66e4d865b3537a9a88cccd7107c95"
+                       (openpgp-fingerprint
+                        "2841 9AC6 5038 7440 C7E9  2FFA 2208 D209 58C1 DEB0"))))))
    (service home-shepherd-service-type)
    (service home-zsh-service-type
             (home-zsh-configuration
