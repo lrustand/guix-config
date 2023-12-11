20d5c2da975cbe3ad6096cf84f7128ad4e7e666f
Lars Rustand <rustand.lars@gmail.com>
2023-12-11 21:51:08 +0100

    Add hacky symlink service

---
+++ src/lrustand/services/symlinks.scm
@@ -0,0 +1,35 @@
+(define-module (lrustand services symlinks)
+  #:use-module (gnu)
+  #:use-module (gnu services)
+  #:use-module (gnu home services)
+  #:export (home-symlinks-service-type))
+
+(define (symlink-files lst)
+  (with-imported-modules '((guix build utils))
+    #~(begin
+        (use-modules (guix build utils))
+        (for-each
+         (lambda (it)
+          (let ((from-file (car it))
+                (target-file (string-append #$(getenv "HOME") "/"
+                                            (cadr it))))
+            (mkdir-p (dirname target-file))
+            (if (and (file-exists? target-file)
+                     (equal? 'symlink (stat:type (lstat target-file)))
+                     (equal? (canonicalize-path target-file)
+                             (canonicalize-path from-file)))
+                (format #t "~a is already a symlink to ~a!\n"
+                        target-file from-file)
+                (symlink from-file
+                         target-file))))
+         '#$lst))))
+
+(define home-symlinks-service-type
+  (service-type
+   (name 'symlink)
+   (extensions
+    (list (service-extension home-activation-service-type
+                             symlink-files)))
+   (default-value '(()))
+   (description
+    "")))
