f9b4b405eb5db367754c9814a9f36170f544ecf2
Lars Rustand <rustand.lars@gmail.com>
2023-12-26 21:52:15 +0100

    Orderless

--- files/emacs/init.el
+++ files/emacs/init.el
@@ -253,6 +253,14 @@
   :init
   (vertico-mode))
 
+(use-package orderless
+  :init
+  ;; Configure a custom style dispatcher (see the Consult wiki)
+  ;; (setq orderless-style-dispatchers '(+orderless-consult-dispatch orderless-affix-dispatch)
+  ;;       orderless-component-separator #'orderless-escapable-split-on-space)
+  (setq completion-styles '(orderless emacs22)
+        completion-category-defaults nil
+        completion-category-overrides '((file (styles partial-completion)))))
 
 (use-package bibtex-completion
   :ensure t
