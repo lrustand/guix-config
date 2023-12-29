b67648bef161e1e61d83375056511951a8870548
Lars Rustand <rustand.lars@gmail.com>
2023-12-29 20:26:26 +0100

    Inherit from base in drit

--- src/lrustand/systems/drit.scm
+++ src/lrustand/systems/drit.scm
@@ -1,83 +1,18 @@
 (define-module (lrustand systems drit)
-  #:use-module (gnu)
-  #:use-module (nongnu packages linux)
-  #:use-module (nongnu system linux-initrd)
-  #:use-module (gnu packages firmware)
-  #:use-module (gnu packages shells)
-  #:use-module (gnu services xorg)
-
-  #:use-module (gnu services networking)
-  #:use-module (gnu services virtualization)
-  #:use-module (gnu services ssh)
-  #:use-module (gnu services admin)
-  #:use-module (gnu services desktop))
+  #:use-module (lrustand systems base)
+  #:use-module (lrustand services base)
+  #:use-module (gnu system file-systems)
+  #:use-module (gnu system))
 
 (define-public %drit-operating-system
-  (operating-system
-    (locale "en_US.utf8")
-    (timezone "Europe/Oslo")
-    (keyboard-layout (keyboard-layout "us,no" #:options '("grp:switch")))
+  (operating-system (inherit %base-operating-system)
     (host-name "drit")
-
-    (kernel linux)
-    (initrd microcode-initrd)
-    ;;(initrd-modules (append '("vmd") %base-initrd-modules))
-    (firmware (list
-               linux-firmware
-               sof-firmware))
-
-    ;;(kernel-arguments (list "console=ttyS0,115200"))
-
-    (users
-      (cons*
-        (user-account
-          (name "lars")
-          (comment "")
-          (group "users")
-          (shell (file-append zsh "/bin/zsh"))
-          (home-directory "/home/lars")
-          (supplementary-groups '("wheel" "input")))
-        %base-user-accounts))
-
-    (packages
-      (append
-        (list sof-firmware)
-        (map specification->package
-          (list
-            "nss-certs"
-            "neovim"
-            "htop"
-            "tmux"
-            "stumpwm"
-            "xinitrc-xsession"
-            "libinput"
-            "xf86-input-libinput"
-            "openssh"))
-        %base-packages))
-
-    (services
-      (cons*
-       (service qemu-binfmt-service-type
-                (qemu-binfmt-configuration
-                 (platforms (lookup-qemu-platforms "arm" "aarch64"))))
-       (set-xorg-configuration
-         (xorg-configuration
-           (keyboard-layout keyboard-layout)))
-       (modify-services %desktop-services
-         (guix-service-type config => (guix-configuration
-           (inherit config)
-           (substitute-urls
-            (append (list "https://substitutes.nonguix.org")
-              %default-substitute-urls))
-           (authorized-keys
-            (append (list (plain-file "non-guix.pub" "(public-key (ecc (curve Ed25519) (q #C1FD53E5D4CE971933EC50C9F307AE2171A2D3B52C804642A7A35F84F3A4EA98#)))"))
-              %default-authorized-guix-keys)))))))
 
      (bootloader
        (bootloader-configuration
          (bootloader grub-bootloader)
          (targets '("/dev/sda"))
-         (keyboard-layout keyboard-layout)))
+         (keyboard-layout %lr/keyboard-layout)))
 
      (file-systems
       (cons*
