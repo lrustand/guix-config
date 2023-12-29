7694cf8e65c4dbf4b86286c06eadcf8e8d47ab0f
Lars Rustand <rustand.lars@gmail.com>
2023-12-29 19:31:46 +0100

    Inherit yoga from base-operating-system

--- src/lrustand/systems/yoga.scm
+++ src/lrustand/systems/yoga.scm
@@ -1,83 +1,26 @@
 (define-module (lrustand systems yoga)
-  #:use-module (gnu)
-  #:use-module (nongnu packages linux)
-  #:use-module (nongnu system linux-initrd)
-  #:use-module (gnu packages firmware)
-  #:use-module (gnu packages shells)
-  #:use-module (gnu services xorg)
+  #:use-module (lrustand systems base)
+  #:use-module (lrustand services base)
+  #:use-module (gnu system)
+  #:use-module (gnu system file-systems)
+  #:use-module (gnu system linux-initrd)
+  #:use-module (gnu services)
+  #:use-module (gnu services virtualization))
 
-  #:use-module (gnu services networking)
-  #:use-module (gnu services virtualization)
-  #:use-module (gnu services ssh)
-  #:use-module (gnu services admin)
-  #:use-module (gnu services desktop))
-
-(define %yoga-operating-system
-  (operating-system
-    (locale "en_US.utf8")
-    (timezone "Europe/Oslo")
-    (keyboard-layout (keyboard-layout "us,no" #:options '("grp:switch")))
+(define-public %yoga-operating-system
+  (operating-system (inherit %base-operating-system)
     (host-name "yoga")
   
-    (kernel linux)
-    (initrd microcode-initrd)
     (initrd-modules (append '("vmd") %base-initrd-modules))
-    (firmware (list
-               linux-firmware
-               sof-firmware))
   
     ;;(kernel-arguments (list "console=ttyS0,115200"))
-  
-    (users
-      (cons*
-        (user-account
-          (name "lars")
-          (comment "")
-          (group "users")
-          (shell (file-append zsh "/bin/zsh"))
-          (home-directory "/home/lars")
-          (supplementary-groups '("wheel" "input")))
-        %base-user-accounts))
-  
-    (packages
-      (append
-        (list sof-firmware)
-        (map specification->package
-          (list
-            "nss-certs"
-            "neovim"
-            "htop"
-            "tmux"
-            "stumpwm"
-            "xinitrc-xsession"
-            "libinput"
-            "xf86-input-libinput"
-            "openssh"))
-        %base-packages))
   
     (services
       (cons*
        (service qemu-binfmt-service-type
                 (qemu-binfmt-configuration
                  (platforms (lookup-qemu-platforms "arm" "aarch64"))))
-       (set-xorg-configuration
-         (xorg-configuration
-           (keyboard-layout keyboard-layout)))
-       (modify-services %desktop-services
-         (guix-service-type config => (guix-configuration
-           (inherit config)
-           (substitute-urls
-            (append (list "https://substitutes.nonguix.org")
-              %default-substitute-urls))
-           (authorized-keys
-            (append (list (plain-file "non-guix.pub" "(public-key (ecc (curve Ed25519) (q #C1FD53E5D4CE971933EC50C9F307AE2171A2D3B52C804642A7A35F84F3A4EA98#)))"))
-              %default-authorized-guix-keys)))))))
-  
-     (bootloader
-       (bootloader-configuration
-         (bootloader grub-efi-bootloader)
-         (targets '("/boot/efi"))
-         (keyboard-layout keyboard-layout)))
+       %lr/desktop-services))
   
      (file-systems
       (cons*
@@ -91,6 +34,7 @@
          (mount-point "/boot/efi")
          (device (uuid "2C6E-7735"
                        'fat32))
-         (type "vfat")) %base-file-systems))))
+         (type "vfat"))
+       %base-file-systems))))
 
 %yoga-operating-system
