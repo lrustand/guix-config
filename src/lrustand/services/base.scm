1628d2443162e8179b7b9b9b7f8b3697d5f83028
Lars Rustand <rustand.lars@gmail.com>
2023-12-31 00:13:49 +0100

    Add %lr/base-services

--- src/lrustand/services/base.scm
+++ src/lrustand/services/base.scm
@@ -10,6 +10,18 @@
 (define-public %lr/keyboard-layout
   (keyboard-layout "us,no" #:options '("grp:switch")))
 
+(define-public %lr/base-services
+  (cons*
+    (modify-services %base-services
+      (guix-service-type config => (guix-configuration
+        (inherit config)
+        (substitute-urls
+         (append (list "https://substitutes.nonguix.org")
+           %default-substitute-urls))
+        (authorized-keys
+         (append (list (plain-file "non-guix.pub" "(public-key (ecc (curve Ed25519) (q #C1FD53E5D4CE971933EC50C9F307AE2171A2D3B52C804642A7A35F84F3A4EA98#)))"))
+           %default-authorized-guix-keys)))))))
+
 (define-public %lr/desktop-services
   (cons*
     (set-xorg-configuration
