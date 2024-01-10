10b29ba8508ebfef0c9ba77755138bbfe3e92caa
Lars Rustand <rustand.lars@gmail.com>
2024-01-10 17:26:12 +0100

    dmenu touch gesture

--- home.scm
+++ home.scm
@@ -229,7 +229,7 @@
                               "-g" "2,LR,*,*,R,xdotool key Super_L+p"
                               "-g" "1,DU,B,*,P,xdotool key Hyper_L+m"
                               "-g" "1,UD,B,*,P,xdotool key Hyper_L+M"
-                              "-g" "1,UD,T,*,P,SHOW_MENU"
+                              "-g" "1,UD,T,*,P,rofi -show drun -show-icons -icon-theme Papirus"
                               "-g" "1,DLUR,TR,*,P,xdotool key Super_L+f"))))
 
    (service home-xdg-configuration-files-service-type
