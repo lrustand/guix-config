150dd0e32126c89311c62c22d4a33d84df84524a
Lars Rustand <rustand.lars@gmail.com>
2024-01-12 18:05:56 +0100

    Improve pinephone image partitioning

--- src/lrustand/systems/pinephone-pro.scm
+++ src/lrustand/systems/pinephone-pro.scm
@@ -3,10 +3,12 @@
   #:use-module (lrustand systems base)
   #:use-module (lrustand services base)
   #:use-module (lrustand services grow-part)
+  #:use-module (gnu)
   #:use-module (gnu image)
   #:use-module (gnu packages)
   #:use-module (gnu system)
   #:use-module (gnu system file-systems)
+  #:use-module (gnu system image)
   #:use-module (gnu system images rock64)
   #:use-module (gnu system linux-initrd)
   #:use-module (gnu system shadow)
@@ -17,10 +19,9 @@
   #:use-module (gnu services ssh)
   #:use-module (gnu services networking)
   #:use-module (gnu services xorg)
-  #:use-module (gnu services base)
   #:use-module (gnu services sound)
   #:use-module (gnu services desktop)
-  #:use-module (srfi srfi-1)
+  #:use-module (srfi srfi-26)
   #:use-module (guix gexp)
   #:use-module (guix platforms arm))
 
@@ -35,7 +36,7 @@
        "console=ttyS2,115200"
        "earlycon=uart8250,mmio32,0xff1a0000"
        "earlyprintk")
-      (drop-right %default-kernel-arguments 1)))
+      %default-kernel-arguments))
 
     (initrd base-initrd)
     (initrd-modules '())
@@ -84,11 +85,32 @@
       %lr/wifi-networks-services
       %lr/base-services))))
 
-(define-public pinephone-pro-image
+;; TODO All this shit below is untested
+(define pinephone-pro-root-partition
+  (partition
+   (size 'guess)
+   (offset (expt 2 24))
+   (label "ppp-root")
+   (flags '(boot))
+   (initializer (gexp initialize-root-partition))))
+
+(define pinephone-pro-image
+  (image-without-os
+   (format 'disk-image)
+   (partitions (list pinephone-pro-root-partition))))
+
+(define-public pinephone-pro-image-type
+  (image-type
+   (name 'pinephone-pro-raw)
+   (constructor (cut image-with-os pinephone-pro-image <>))))
+
+(define-public pinephone-pro-barebones-raw-image
   (image
    (inherit
     (os+platform->image %pinephone-pro-operating-system aarch64-linux
-                        #:type rock64-image-type))
-   (name 'pinephone-pro-image)))
+                        #:type pinephone-pro-image-type))
+   (name 'pinephone-pro-barebones-raw-image)))
 
-pinephone-pro-image
+;; Return the default image.
+pinephone-pro-barebones-raw-image
+
