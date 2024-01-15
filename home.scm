daf27c96fc0265107e485457c354ce8614030a3e
Lars Rustand <rustand.lars@gmail.com>
2024-01-15 21:17:02 +0100

    Temporarily disable problematic services

--- home.scm
+++ home.scm
@@ -206,20 +206,20 @@
 
    (service home-syncthing-service-type)
 
-   (service home-offlineimap-service-type)
+   ;;(service home-offlineimap-service-type)
 
-   (service home-lisgd-service-type
-            (lisgd-configuration
-             (home-service? #t)
-             (extra-options '("-d" "/dev/input/by-path/pci-0000:00:15.0-platform-i2c_designware.0-event"
-                              "-g" "1,RL,R,*,P,xdotool key Super_L+n"
-                              "-g" "1,LR,L,*,P,xdotool key Super_L+p"
-                              "-g" "2,RL,*,*,R,xdotool key Super_L+n"
-                              "-g" "2,LR,*,*,R,xdotool key Super_L+p"
-                              "-g" "1,DU,B,*,P,xdotool key Hyper_L+m"
-                              "-g" "1,UD,B,*,P,xdotool key Hyper_L+M"
-                              "-g" "1,UD,T,*,P,rofi -show drun -show-icons -icon-theme Papirus"
-                              "-g" "1,DLUR,TR,*,P,xdotool key Super_L+f"))))
+   ;;(service home-lisgd-service-type
+   ;;         (lisgd-configuration
+   ;;          (home-service? #t)
+   ;;          (extra-options '("-d" "/dev/input/by-path/pci-0000:00:15.0-platform-i2c_designware.0-event"
+   ;;                           "-g" "1,RL,R,*,P,xdotool key Super_L+n"
+   ;;                           "-g" "1,LR,L,*,P,xdotool key Super_L+p"
+   ;;                           "-g" "2,RL,*,*,R,xdotool key Super_L+n"
+   ;;                           "-g" "2,LR,*,*,R,xdotool key Super_L+p"
+   ;;                           "-g" "1,DU,B,*,P,xdotool key Hyper_L+m"
+   ;;                           "-g" "1,UD,B,*,P,xdotool key Hyper_L+M"
+   ;;                           "-g" "1,UD,T,*,P,rofi -show drun -show-icons -icon-theme Papirus"
+   ;;                           "-g" "1,DLUR,TR,*,P,xdotool key Super_L+f"))))
 
    (service home-xdg-configuration-files-service-type
             `(("msmtp/config"
