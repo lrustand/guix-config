6ead0f7d13d8b65151b0da6be675887083c129e2
Lars Rustand <rustand.lars@gmail.com>
2024-09-15 18:32:33 +0200

    Re-enable and improve consult

--- files/emacs/init.el
+++ files/emacs/init.el
@@ -452,15 +452,23 @@ faces immediately.  Calls `custom-theme-set-faces', which see."
   :init
   (marginalia-mode))
 
-;;(use-package consult
-;;  :ensure t
-;;  :bind (("C-x b" . consult-buffer)))
-;;
-;;(use-package consult-projectile
-;;  :ensure t
-;;  :after consult
-;;  :bind (("C-c p p" . consult-projectile-switch-project)
-;;         ("C-c p f" . consult-projectile)))
+(use-package consult
+  :ensure t
+  :preface
+  (defun my/consult-org-headings ()
+    "Switch to any top-level org heading"
+    (interactive)
+    (consult-org-heading "LEVEL=1"))
+  :custom
+  ;; Disable autmatic previewing
+  (consult-preview-key nil)
+  :bind (("C-x b" . consult-buffer)))
+
+(use-package consult-projectile
+  :ensure t
+  :after consult
+  :bind (("C-c p p" . consult-projectile-switch-project)
+         ("C-c p f" . consult-projectile)))
 
 (use-package orderless
   :ensure t
