4a82a57ecce26e59cafe8f930738192c3cdd59c9
Lars Rustand <rustand.lars@gmail.com>
2024-12-21 00:46:53 +0100

    Fix geiser load path

--- files/emacs/init.el
+++ files/emacs/init.el
@@ -821,17 +821,7 @@ targets."
   :ensure t
   :after geiser
   :config
-  (add-to-list 'geiser-guile-load-path "/home/lars/code/forks/guix")
-  (add-to-list 'geiser-guile-load-path "/home/lars/code/guix-config/src")
-  (add-to-list 'geiser-guile-load-path "/home/lars/code/forks/nonguix")
-  (add-to-list 'geiser-guile-load-path "/home/lars/code/forks/rde/src"))
-
-(with-eval-after-load "geiser-guile"
-  (add-to-list 'geiser-guile-load-path "/home/lars/code/forks/guix")
-  (add-to-list 'geiser-guile-load-path "/home/lars/code/guix-config/src")
-  (add-to-list 'geiser-guile-load-path "/home/lars/code/forks/nonguix")
-  (add-to-list 'geiser-guile-load-path "/home/lars/code/forks/rde/src"))
-
+  (add-to-list 'geiser-guile-load-path "~/.config/guix/current/share/guile/"))
 
 ;;;; Snippets
 ;;;;----------
