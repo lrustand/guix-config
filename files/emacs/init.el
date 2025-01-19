b61a0dd6d4d3a46ca67849504539a1dc343ead99
Lars Rustand <lars.rustand@semcon.com>
2025-01-19 12:39:40 +0000

    Improve cape completion

--- files/emacs/init.el
+++ files/emacs/init.el
@@ -848,19 +848,21 @@ targets."
   ;; used by `completion-at-point'.  The order of the functions matters, the
   ;; first function returning a result wins.  Note that the list of buffer-local
   ;; completion functions takes precedence over the global list.
-  (add-to-list 'completion-at-point-functions #'cape-dabbrev)
-  ;;(add-to-list 'completion-at-point-functions #'cape-file)
-  ;;(add-to-list 'completion-at-point-functions #'cape-elisp-block)
-  ;;(add-to-list 'completion-at-point-functions #'cape-history)
-  (add-to-list 'completion-at-point-functions #'cape-keyword)
-  ;;(add-to-list 'completion-at-point-functions #'cape-tex)
-  ;;(add-to-list 'completion-at-point-functions #'cape-sgml)
-  ;;(add-to-list 'completion-at-point-functions #'cape-rfc1345)
-  ;;(add-to-list 'completion-at-point-functions #'cape-abbrev)
-  ;;(add-to-list 'completion-at-point-functions #'cape-dict)
-  ;;(add-to-list 'completion-at-point-functions #'cape-elisp-symbol)
-  ;;(add-to-list 'completion-at-point-functions #'cape-line)
-  )
+  ;;(add-to-list 'completion-at-point-functions #'cape-dabbrev)
+  :custom
+  (completion-at-point-functions
+   (list (cape-capf-super #'cape-keyword #'yasnippet-capf)
+         (cape-capf-super #'cape-dict #'cape-dabbrev))))
+  ;; #'cape-file)
+  ;; #'cape-elisp-block)
+  ;; #'cape-history)
+  ;; #'cape-tex)
+  ;; #'cape-sgml)
+  ;; #'cape-rfc1345)
+  ;; #'cape-abbrev)
+  ;; #'cape-dict)
+  ;; #'cape-elisp-symbol)
+  ;; #'cape-line)
 
 ;; Completion for shell commands
 (use-package pcmpl-args
